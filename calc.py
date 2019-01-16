6f5e779c1c6796189a79d557167a142cac8b9e41
katomasahiro10 <katomasahiro10@gmail.com>
2019-01-17 00:02:31 +0900

    単語を足したり引いたり

--- calc.py
+++ calc.py
@@ -3,16 +3,37 @@
 from gensim.models import word2vec
 model = word2vec.Word2Vec.load("./wiki.model")
 
-def getvec(keyword):
-    """
-    引数の単語のベクトルを返す関数
-    """
+def is_exist(keyword):
     try:
-        return(model[keyword])
+        model[keyword]
+        return True
+    except:
+        return False
+
+def posi_posi(posi_list):
+    try:
+        results = model.wv.most_similar(positive=posi_list)
+        synonim = []
+        for result in results:
+            synonim.append(result[0])
+        return(synonim)
+    except:
+        return False
+
+def posi_nega(posi_list,nega_list):
+    try:
+        results = model.wv.most_similar(positive=posi_list,negative=nega_list)
+        synonim = []
+        for result in results:
+            synonim.append(result[0])
+        return(synonim)
     except:
         return False
 
 while(True):
-    word = input('キーワードを入力:')
-    word = str(word)
-    print(getvec(word))
+    posi=input('positive keyword list:')
+    posi = posi.split(",")
+    nega=input('negative keyword list:')
+    nega = nega.split(",")
+    print(posi_posi(posi))
+    print(posi_nega(posi,nega))
