51b63f5f4ee260175d64d5e8d738f2ec1c01ac01
katomasahiro10 <katomasahiro10@gmail.com>
2019-01-17 15:02:39 +0900

    構文解析のプログラム
    関数化した

--- parsing.py
+++ parsing.py
@@ -1,35 +1,34 @@
 #coding:utf-8
-import re
 import sys
 args = sys.argv
 
-string = args[1] #引数にとった文字列
-if string[0] != '+' and string[0] != '-':
-    string='+' + string
+def parsing(string):
+    if string[0] != '+' and string[0] != '-':
+        string='+' + string
+    posilist=['']
+    posi_n = 0
+    negalist=['']
+    nega_n = 0
+    for i in string:
+        if i == '+':
+            posilist.append('')
+            posi_n += 1
+            flag = 'p'
+        elif i == '-':
+            negalist.append('')
+            nega_n += 1
+            flag = 'n'
+        else:
+            if flag == 'p':
+                posilist[posi_n -1] += i
+            elif flag == 'n':
+                negalist[nega_n -1] += i
 
+    posilist=posilist[:-1]
+    negalist=negalist[:-1]
 
-posilist=['']
-posi_n = 0
-negalist=['']
-nega_n = 0
+    return(posilist,negalist)
 
-for i in string:
-    if i == '+':
-        posilist.append('')
-        posi_n += 1
-        flag = 'p'
-    elif i == '-':
-        negalist.append('')
-        nega_n += 1
-        flag = 'n'
-    else:
-        if flag == 'p':
-            posilist[posi_n -1] += i
-        elif flag == 'n':
-            negalist[nega_n -1] += i
-
-posilist=posilist[:-1]
-negalist=negalist[:-1]
-
-print("posilist:",posilist)
-print("negalist:",negalist)
+if __name__=='__main__':
+    string = args[1]
+    print(parsing(string))
