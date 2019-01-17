71c7221ccbc701cdfc0bec7798d10b93db577e43
katomasahiro10 <katomasahiro10@gmail.com>
2019-01-17 15:01:56 +0900

    計算できるようにした

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 
 from gensim.models import word2vec
+from parsing import parsing
 model = word2vec.Word2Vec.load("./wiki.model")
 
 def is_exist(keyword):
@@ -10,9 +11,34 @@ def is_exist(keyword):
     except:
         return False
 
+def exist(wordlist):
+    del_key = []
+
+    for i in range(len(wordlist)):
+        if is_exist(wordlist[i]):
+            pass
+        else:
+            del_key.append(i)
+
+    for i in del_key:
+        del(wordlist[i])
+
+    return wordlist
+
+
 def posi_posi(posi_list):
     try:
         results = model.wv.most_similar(positive=posi_list)
+        synonim = []
+        for result in results:
+            synonim.append(result[0])
+        return(synonim)
+    except:
+        return False
+
+def nega_nega(nega_list):
+    try:
+        results = model.wv.most_similar(negative=nega_list)
         synonim = []
         for result in results:
             synonim.append(result[0])
@@ -31,9 +57,16 @@ def posi_nega(posi_list,nega_list):
         return False
 
 while(True):
-    posi=input('positive keyword list:')
-    posi = posi.split(",")
-    nega=input('negative keyword list:')
-    nega = nega.split(",")
-    print(posi_posi(posi))
-    print(posi_nega(posi,nega))
+    string=input('計算式:')
+    posi_list,nega_list = parsing(string)
+    posi_list=exist(posi_list)
+    nega_list=exist(nega_list)
+
+    if(len(posi_list)==0):
+        print(nega_nega(nega_list))
+    elif(len(nega_list)==0):
+        print(posi_posi(posi_list))
+    else:
+        print(posi_nega(posi_list,nega_list))
+
+    print("---")
